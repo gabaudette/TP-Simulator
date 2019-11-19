3bb3e71c232107dee38ddc96660f2ed272000ec1
Gabriel Audette <gabaudette@gmail.com>
2019-11-19 12:50:33 -0500

    Added simulator GUI & map ressource

--- TP-Simulator/TP-Simulator/Program.cs
+++ TP-Simulator/TP-Simulator/Program.cs
@@ -16,7 +16,7 @@ namespace TP_Simulator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SimulatorGUI());
         }
     }
 }
