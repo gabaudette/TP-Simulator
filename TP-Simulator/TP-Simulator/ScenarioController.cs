c71427200f728e9e248b8ecc896529eb44dbfbc4
Gabriel Audette <gabaudette@gmail.com>
2019-11-29 09:23:15 -0500

    Initial Commit

---
+++ TP-Simulator/TP-Simulator/ScenarioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TP_Simulator
+{
+    static class ScenarioController
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SimulatorGUI());
+        }
+    }
+
+}
