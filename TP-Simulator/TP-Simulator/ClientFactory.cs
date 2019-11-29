f60d62eea5509c1c3a3a44db352fa67e9d0857f1
Gabriel Audette <gabaudette@gmail.com>
2019-11-29 10:24:12 -0500

    Create ClientFactory.cs

---
+++ TP-Simulator/TP-Simulator/ClientFactory.cs
@@ -0,0 +1,41 @@
+﻿namespace TP_Simulator
+{
+    public sealed class ClientFactory
+    {
+        private static ClientFactory clientFactory;
+
+        private ClientFactory() { }
+
+        public static ClientFactory GetClientFactory()
+        {
+            if (clientFactory == null)
+                clientFactory = new ClientFactory();
+            return clientFactory;
+        }
+
+        public static Client CreateRescueTeam()
+        {
+            return new RescueTeam();
+        }
+
+        public static Client CreateFire()
+        {
+            return new Fire();
+        }
+
+        public static Client CreatePassenger()
+        {
+            return new Passenger();
+        }
+
+        public static Client CreateMarchandise()
+        {
+            return new Marchandise();
+        }
+
+        public static Client CreateObserver()
+        {
+            return new Observer();
+        }
+    }
+}
