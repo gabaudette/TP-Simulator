bbca43a4ed8dca5be4fa4fe1021a75bcfb6031f9
Gabriel Audette <gabaudette@gmail.com>
2019-11-29 10:24:03 -0500

    Create Client.cs

---
+++ TP-Simulator/TP-Simulator/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace TP_Simulator
+{
+    public abstract class Client
+    {
+        public string Name { get; protected set; }
+        public Client(string name)
+        {
+            Name = name;
+        }
+    }
+}
