58e2b0df5595d4c36d7e818e0c38adee36d1032c
Gabriel Audette <gabaudette@gmail.com>
2019-11-29 10:24:21 -0500

    Create Fire.cs

---
+++ TP-Simulator/TP-Simulator/Fire.cs
@@ -0,0 +1,11 @@
+﻿namespace TP_Simulator
+{
+    class Fire : Client
+    {
+        public int FireSpan { get; private set; }
+        public Fire(string name, int fireSpan) : base(name)
+        {
+            FireSpan = fireSpan;
+        }
+    }
+}
