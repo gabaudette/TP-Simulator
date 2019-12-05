9a67e9169c844035e459e5e1aac1dc03a0f6a0b9
Gabriel Audette <gabaudette@gmail.com>
2019-12-05 11:46:30 -0500

    Update MaintenanceSate.cs

--- TP-Simulator/TP-Simulator/MaintenanceSate.cs
+++ TP-Simulator/TP-Simulator/MaintenanceSate.cs
@@ -2,7 +2,6 @@
 {
     class MaintenanceSate : StateAircraft
     {
-
         protected override void Do()
         {
             //base.aircraft.Maintenance = 0; TODO:Access maintenance time of the aircraft
