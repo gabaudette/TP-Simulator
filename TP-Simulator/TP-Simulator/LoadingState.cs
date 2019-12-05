3c857096e576046abe02e80c4260cb5fccec8cf1
Gabriel Audette <gabaudette@gmail.com>
2019-12-04 21:55:01 -0500

    Create LoadingState.cs

---
+++ TP-Simulator/TP-Simulator/LoadingState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_Simulator
+{
+    class LoadingState : StateAircraft
+    {
+        protected override void Do()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
