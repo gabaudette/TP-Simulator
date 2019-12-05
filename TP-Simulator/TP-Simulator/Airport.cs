d08ec562673a9258b7ae29e10e0ca4137ea28c8a
Gabriel Audette <gabaudette@gmail.com>
2019-12-04 19:30:22 -0500

    Update Airport.cs

--- TP-Simulator/TP-Simulator/Airport.cs
+++ TP-Simulator/TP-Simulator/Airport.cs
@@ -4,10 +4,20 @@ namespace TP_Simulator
 {
     public class Airport
     {
-
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int MinPassenger { get; set; }
+        public int MaxPassenger { get; set; }
+        public int MinMarchandise { get; set; }
+        public int MaxMarchandise { get; set; }
         public List<Aircraft> Aircrafts { get; set; }
 
         public Airport() { }
-
+        
+        public void Reviver(Scenario scenario)
+        {
+            
+        }
     }
 }
