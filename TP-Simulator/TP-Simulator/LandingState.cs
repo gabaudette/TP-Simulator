e41553b19e3b9ed50edc3aea5d93d9068b1954b8
Gabriel Audette <gabaudette@gmail.com>
2019-12-04 21:55:00 -0500

    Create LandingState.cs

---
+++ TP-Simulator/TP-Simulator/LandingState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_Simulator
+{
+    class LandingState : StateAircraft
+    {
+        protected override void Do()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
