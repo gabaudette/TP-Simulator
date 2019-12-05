b9ecd4b4931c7d4be48e2bf27dd4e49894dc5b9d
Gabriel Audette <gabaudette@gmail.com>
2019-12-04 21:55:10 -0500

    Create UnloadingState.cs

---
+++ TP-Simulator/TP-Simulator/UnloadingState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TP_Simulator
+{
+    class UnloadingState : StateAircraft
+    {
+        protected override void Do()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
