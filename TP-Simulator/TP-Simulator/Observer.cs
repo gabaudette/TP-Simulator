b0c80836edc23a76ee652136c2c39483cd114598
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 17:01:01 -0500

    Update Observer.cs

--- TP-Simulator/TP-Simulator/Observer.cs
+++ TP-Simulator/TP-Simulator/Observer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TP_Simulator
 {
-    class Observer : PositionableAirportClient
+    class Observer : PositionableClient
     {
         public Observer(int posX, int posY) : base(posX,posY) { }
     }
