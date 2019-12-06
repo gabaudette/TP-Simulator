52d61829ed195ca27b83c7eda4743811f6846c4d
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 16:54:52 -0500

    Update Marchandise.cs

--- TP-Simulator/TP-Simulator/Marchandise.cs
+++ TP-Simulator/TP-Simulator/Marchandise.cs
@@ -3,7 +3,7 @@
     class Marchandise : AirportClient
     {
         public int Weight { get; private set; }
-        public Marchandise(int amount,Airport airport, Airport destination) : base(amount, airport,destination){ }
+        public Marchandise(int amount, Airport airport, Airport destination) : base(amount, airport, destination) { }
 
         public override string ToString()
         {
