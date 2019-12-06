14e5ad1a110656f0668168b1d45fb8b85a9bc6bf
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 16:59:49 -0500

    Update Timer.cs

--- TP-Simulator/TP-Simulator/Timer.cs
+++ TP-Simulator/TP-Simulator/Timer.cs
@@ -4,13 +4,14 @@
     {
         public int CurrentMinute{ get; set; }
         public int CurrentHour { get; set; }
+
         public Timer()
         {
             CurrentHour = 0;
             CurrentMinute = 0;
         }
 
-        public void addTick ()
+        public void AddTick()
         {
             if (CurrentMinute == 45)
             {
@@ -21,14 +22,12 @@
                 CurrentMinute += 15;
         }
 
-
         public override string ToString()
         {
             if (CurrentMinute == 0)
                 return $"{CurrentHour} : {CurrentMinute}0";
             else
                 return $"{CurrentHour} : {CurrentMinute}";
-            
         }
     }
 }
