f9a3a3c3423b648f2b67a561448f7b8797de01ea
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 17:01:03 -0500

    Create PositionableClient.cs

---
+++ TP-Simulator/TP-Simulator/PositionableClient.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TP_Simulator
+{
+    class PositionableClient : Client
+    {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        public PositionableClient(int posX, int posY)
+        {
+            PosX = posX;
+            PosY = posY;
+        }
+
+
+    }
+}
