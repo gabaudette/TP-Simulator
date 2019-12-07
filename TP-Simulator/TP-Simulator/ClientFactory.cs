1aa34c70eee1bcb7e5c187651c2801ab785384d2
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 19:16:55 -0500

    Update ClientFactory.cs

--- TP-Simulator/TP-Simulator/ClientFactory.cs
+++ TP-Simulator/TP-Simulator/ClientFactory.cs
@@ -12,30 +12,32 @@
                 clientFactory = new ClientFactory();
             return clientFactory;
         }
-        /*
-        public static Client CreateRescueTeam()
+        
+        public static Client CreateRescueTeam(int x, int y)
         {
-            return new RescueTeam();
+            return new RescueTeam(x, y);
         }
         
-        public static Client CreateFire()
+        public static Client CreateFire(int fireSpan, int x, int y)
         {
-            return new Fire();
+            return new Fire(fireSpan, x, y);
         }
 
-        public static Client CreatePassenger()
+        //TODO: Check assign airport (for destination) in scenario (Simulator UML Schema)
+        public static Client CreatePassenger(int amount, Airport airport, Airport destination)
         {
-            return new Passenger();
+            return new Passenger(amount, airport, destination);
         }
 
-        public static Client CreateMarchandise()
+        //TODO: Check assign airport (for destination) in scenario (Simulator UML Schema)
+        public static Client CreateMarchandise(int amount, Airport airport, Airport destination)
         {
-            return new Marchandise();
+            return new Marchandise( amount,airport,destination);
         }
 
-        public static Client CreateObserver()
+        public static Client CreateObserver(int x, int y)
         {
-            return new Observer();
-        }*/
+            return new Observer(x, y);
+        }
     }
 }
