1f1d1b553ada0008ccf5b39264517da45e6e4a0f
Gabriel Audette <gabaudette@gmail.com>
2019-12-06 19:29:59 -0500

    Update StateAircraft.cs

--- TP-Simulator/TP-Simulator/StateAircraft.cs
+++ TP-Simulator/TP-Simulator/StateAircraft.cs
@@ -2,10 +2,8 @@
 {
     public abstract class StateAircraft
     {
-        public Aircraft aircraft { get; protected set; }
         public StateAircraft()
         {
-            //aircraft = new Aircraft();
         }
         protected abstract void Do();
 
