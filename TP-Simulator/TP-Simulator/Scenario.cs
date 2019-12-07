01c6694cf9a3820d4f8a72aa0c012dd5d81440a6
Chris <noa_lafortune@hotmail.com>
2019-12-07 15:43:49 -0500

    Update Scenario.cs

--- TP-Simulator/TP-Simulator/Scenario.cs
+++ TP-Simulator/TP-Simulator/Scenario.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Xml.Serialization;
@@ -14,17 +15,21 @@ namespace TP_Simulator
         [XmlIgnore]
         public bool Pause { get; private set; }
         [XmlIgnore]
-        public System.Random Rnd { get; private set; }
-
+        public Random Rnd { get; private set; }
         public List<Airport> Airports { get; set; }
         public List<Aircraft> FlyingAicrafts { get; set; }
         [XmlIgnore]
         public AirportNotifier AirportNotifier { get; set; }
-
         [XmlIgnore]
         public TickNotifier TickNotifier { get; set; }
-
+        [XmlIgnore]
+        public HourNotifier HourNotifier { get; set; }
         private ClientFactory clientFactory;
+        [XmlIgnore]
+        public PositionableClient LastClient { get; set; }
+
+        [XmlIgnore]
+        Airport closestAirport = new Airport();
 
         private Scenario()
         {
@@ -37,6 +42,7 @@ namespace TP_Simulator
         {
             AirportNotifier = new AirportNotifier(view.OnDeserialize);
             TickNotifier = new TickNotifier(view.OnTick);
+            HourNotifier = new HourNotifier(view.OnHour);
         }
 
         public static Scenario Instance
@@ -84,14 +90,77 @@ namespace TP_Simulator
             Timer.AddTick();
             this.clientFactory = ClientFactory.GetClientFactory();
             if (Timer.HourPassed()) {
-                // ?? Im I doing shit ???
-                //int amount = Rnd.Next(1, 4);
-                //ClientFactory.CreatePassenger();
+                LastClient = new PositionableClient();
+                Rnd = new Random(DateTime.Now.Millisecond);
+                int nbCall;
+                int destinationID;
+                //Observator
+                LastClient = (PositionableClient)ClientFactory.CreateObserver();
+                addClientToAirport(LastClient);
+
+                //Fire
+                nbCall = Rnd.Next(1, 3);
+                for (int i = 0; i < nbCall; i++)
+                {
+                    LastClient = (PositionableClient)ClientFactory.CreateFire();
+                    addClientToAirport(LastClient);
+                }
+
+                //ResuceTeam
+                nbCall = Rnd.Next(1, 2);
+                for (int i = 0; i < nbCall; i++)
+                {
+                    LastClient = (PositionableClient)ClientFactory.CreateRescueTeam();
+                    addClientToAirport(LastClient);
+                }
+
+                //Passenger
+                for (int i = 0; i < Airports.Count; i++)
+                {
+                    destinationID = Rnd.Next(0, Airports.Count);
+                    ClientFactory.CreatePassenger(Airports[i], Airports[destinationID]);
+                }
+
+                //Marchandise
+                for (int i = 0; i < Airports.Count; i++)
+                {
+                    destinationID = Rnd.Next(0, Airports.Count);
+                    ClientFactory.CreateMarchandise(Airports[i], Airports[destinationID]);
+                }
+
+                HourNotifier();
             }
 
             TickNotifier();
         }
 
-        //?
+        private void addClientToAirport(PositionableClient paramClient)
+        {
+            
+            closestAirport = getClosestAirport(paramClient.PosX, paramClient.PosY);
+            closestAirport.Clients.Add(paramClient);
+        }
+
+        public Airport getClosestAirport(int posX,int posY)
+        {
+            Airport closestAirport = new Airport();
+
+            double distance = 10000;
+
+            for (int i = 0; i < Airports.Count; i++)
+            {
+               double newDistance = Math.Sqrt((Math.Pow(Airports[i].Y - posY,2)) + (Math.Pow(Airports[i].X - posX, 2)));
+
+                if (newDistance < distance)
+                {
+                    distance = newDistance;
+                    closestAirport = Airports[i];
+                }
+                
+            }
+
+            return closestAirport;
+        }
+
     }
 }
