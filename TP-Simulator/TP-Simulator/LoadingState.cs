6de4f8a13d41660cab5615ce2cffc7badc741c93
Chris <noa_lafortune@hotmail.com>
2019-12-07 15:43:57 -0500

    Update LoadingState.cs

--- TP-Simulator/TP-Simulator/LoadingState.cs
+++ TP-Simulator/TP-Simulator/LoadingState.cs
@@ -3,9 +3,15 @@
     //TODO ? Create a new Sup Class (Passenger/Cargo Plane State)
     class LoadingState : StateAircraft
     {
-        public override void Do()
+        public override void Do(Aircraft aircraft)
         {
+            if(aircraft)
+            ChangeState(aircraft);
+        }
 
+        public override void ChangeState(Aircraft aircraft)
+        {
+            aircraft.CurrentState = new TakeOffState();
         }
 
         public override string ToString()
