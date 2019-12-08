17baa0450b687754d89445c1be485279c5d21dfc
Chris <noa_lafortune@hotmail.com>
2019-12-08 15:05:56 -0500

    Update FlyingState.cs

--- TP-Simulator/TP-Simulator/FlyingState.cs
+++ TP-Simulator/TP-Simulator/FlyingState.cs
@@ -4,19 +4,25 @@ namespace TP_Simulator
 {
     class FlyingState : StateAircraft
     {
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
         public override void Do(Aircraft aircraft)
         {
+            aircraft.moveAicraft(aircraft);
+
             ChangeState(aircraft);
         }
 
         public override void ChangeState(Aircraft aircraft)
         {
-            aircraft.CurrentState = new LandingState();
+            aircraft.CurrentState = new UnloadingState();
         }
 
         public override string ToString()
         {
             return $"En Vol";
         }
+
     }
 }
