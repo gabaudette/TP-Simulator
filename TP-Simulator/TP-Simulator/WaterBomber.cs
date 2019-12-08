98f63964bf3ec8957f017ea3805811caf3f41c7b
Chris <noa_lafortune@hotmail.com>
2019-12-07 19:53:51 -0500

    Update WaterBomber.cs

--- TP-Simulator/TP-Simulator/WaterBomber.cs
+++ TP-Simulator/TP-Simulator/WaterBomber.cs
@@ -4,5 +4,14 @@
     {
         public int WaterCapacity { get; set; }
         public int DroppingTime { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Name},WaterBomber,{CurrentState},N/A";
+        }
+
+
+
     }
 }
