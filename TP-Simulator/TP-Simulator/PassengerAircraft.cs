e0110a306c801f1448efd411cd002f979ee48602
Chris <noa_lafortune@hotmail.com>
2019-12-08 15:06:03 -0500

    Update PassengerAircraft.cs

--- TP-Simulator/TP-Simulator/PassengerAircraft.cs
+++ TP-Simulator/TP-Simulator/PassengerAircraft.cs
@@ -1,8 +1,35 @@
-﻿namespace TP_Simulator
+﻿using System;
+using System.Xml.Serialization;
+
+namespace TP_Simulator
 {
     public class PassengerAircraft : Aircraft
     {
         public int LoadingTime { get; set; }
         public int UnloadingTime { get; set; }
+        public int MaxCapacity { get; set; }
+        [XmlIgnore]
+        public Airport Destination{ get; set; }
+        [XmlIgnore]
+        public int CurrentCapacity { get; set; }
+
+        public override void moveAicraft(Aircraft aircraft)
+        {
+
+            PassengerAircraft passAircraft = (PassengerAircraft)aircraft;
+            Airport destAirport = passAircraft.Destination;
+            FlyingState position = (FlyingState)passAircraft.CurrentState;
+            double distance = Math.Sqrt((Math.Pow(destAirport.Y - position.PosY, 2)) + (Math.Pow(destAirport.X - position.PosX, 2)));
+
+            double tickRequired = distance / (passAircraft.Speed * 15);
+            double XbyTick = (destAirport.X - position.PosX) / tickRequired;
+            double YbyTick = (destAirport.Y - position.PosY) / tickRequired;
+
+
+            position.PosX = position.PosX + Convert.ToInt32(XbyTick);
+            position.PosY = position.PosY + Convert.ToInt32(YbyTick);
+
+        }
+
     }
 }
