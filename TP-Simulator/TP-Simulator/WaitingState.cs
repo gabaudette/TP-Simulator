b4472c9d8831807f368ea25bbb3e2d5a8c49ae46
Chris <noa_lafortune@hotmail.com>
2019-12-08 15:05:52 -0500

    Update WaitingState.cs

--- TP-Simulator/TP-Simulator/WaitingState.cs
+++ TP-Simulator/TP-Simulator/WaitingState.cs
@@ -5,13 +5,40 @@
         //TODO ? Create a new Sup Class (Passenger/Cargo Plane State)
         public override void Do(Aircraft aircraft)
         {
-            ChangeState(aircraft);
+            PassengerAircraft passAircraft = (PassengerAircraft)aircraft;
+
+            for (int i = 0; i < passAircraft.airport.Clients.Count; i++)
+            {
+                AirportClient airClient = (AirportClient)passAircraft.airport.Clients[i];
+
+                if (passAircraft.CurrentCapacity == 0)
+                    passAircraft.Destination = airClient.Destination;
+
+                if (airClient.Destination == passAircraft.Destination)
+                {
+                    if (airClient.Amount > passAircraft.CurrentCapacity)
+                    {
+                        passAircraft.CurrentCapacity += airClient.Amount;
+                        airClient.Amount = airClient.Amount - passAircraft.CurrentCapacity;
+                    }
+                    else
+                    {
+                        passAircraft.CurrentCapacity += airClient.Amount;
+                        passAircraft.airport.Clients.Remove(passAircraft.airport.Clients[i]);
+                    }
+                }
+            }
+
+            if (passAircraft.MaxCapacity == 0)
+            {
+                ChangeState(passAircraft);
+            }
         }
 
         
         public override void ChangeState(Aircraft aircraft)
         {
-            aircraft.CurrentState = new TakeOffState();
+            aircraft.CurrentState = new LoadingState();
         }
 
         public override string ToString()
