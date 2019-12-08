23401c0ec992c01701988fb2eb88bf4d69b3dcc7
Chris <noa_lafortune@hotmail.com>
2019-12-07 19:53:57 -0500

    Update RescueTeam.cs

--- TP-Simulator/TP-Simulator/RescueTeam.cs
+++ TP-Simulator/TP-Simulator/RescueTeam.cs
@@ -3,5 +3,10 @@
     class RescueTeam : PositionableClient
     {
         public RescueTeam(int posX, int posY) : base(posX, posY) { }
+
+        public override string ToString()
+        {
+            return $"Secours en {PosX},{PosY}";
+        }
     }
 }
