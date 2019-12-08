02b84be10e6c6d4e1d4b825d8f0d62d3155a3316
Chris <noa_lafortune@hotmail.com>
2019-12-07 19:54:09 -0500

    Create WaitingState.cs

---
+++ TP-Simulator/TP-Simulator/WaitingState.cs
@@ -0,0 +1,22 @@
+﻿namespace TP_Simulator
+{
+    class WaitingState : StateAircraft
+    {
+        //TODO ? Create a new Sup Class (Passenger/Cargo Plane State)
+        public override void Do(Aircraft aircraft)
+        {
+            ChangeState(aircraft);
+        }
+
+        
+        public override void ChangeState(Aircraft aircraft)
+        {
+            aircraft.CurrentState = new TakeOffState();
+        }
+
+        public override string ToString()
+        {
+            return $"En attente";
+        }
+    }
+}
